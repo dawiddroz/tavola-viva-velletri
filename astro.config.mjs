56c375ef5097d4d9b99ea42f408fb46423c658cb
dawiddroz <dawiddroz@users.noreply.github.com>
2026-07-29 17:51:58 +0200

    Fix: base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://tavolaviva.it',
+  site: 'https://dawiddroz.github.io',
+  base: '/tavola-viva-velletri',
   server: {
     allowedHosts: true,
   },
